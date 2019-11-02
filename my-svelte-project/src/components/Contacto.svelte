3babbfc59c01211d17f4e74cd1a29fb0ea7752f4
Israel Ortiz <soy@israelortiz.com>
2019-11-02 15:58:48 -0600

    on:submit svelte

--- my-svelte-project/src/components/Contacto.svelte
+++ my-svelte-project/src/components/Contacto.svelte
@@ -1,3 +1,23 @@
+<script>
+  export const sendMail = () => {
+    const email = document.getElementById("email").value;
+    const nombre = document.getElementById("nombre").value;
+    const mensaje = document.getElementById("mensaje").value;
+    const cuerpoemail = `${nombre} con correo ${email} envió un mensaje desde tu página web, ${mensaje}`;
+    console.log(email, nombre, mensaje);
+    // https://sendgrid.com/docs/API_Reference/Web_API/mail.html
+    // api_user = your_sendgrid_username & api_key=your_sendgrid_password & to=destination@example.com& toname=Destination & subject=Example_Subject & text=testingtextbody & from=info@domain.com
+    // const data = { api_user: "your_sendgrid_username", api_key: "your_sendgrid_password", to: "mi@email.com", toname: "myname", subject: "mensaje desde tu portafolio", text=cuerpoemail, from=email}
+    //   axios
+    //     .post("https://api.sendgrid.com/api/mail.send.json", data)
+    //     .then((response) => {
+    //       const emailSended = response.data
+    //       console.log(`POST: response data`, emailSended)
+    //     })
+    //     .catch((error) => console.error(error))
+  };
+</script>
+
 <section class="contacto">
   <div class="container ">
     <div class="row d-flex align-items-center height100vh">
@@ -12,7 +32,7 @@
         </a>
       </div>
       <div class="col">
-        <form onSubmit="event.preventDefault(); sendMail();" method="post">
+        <form on:submit|preventDefault={sendMail} method="post">
           <div class="form-group">
             <label for="exampleInputEmail1">Correo electrónico</label>
             <input
