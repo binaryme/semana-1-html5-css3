154d35d7c16dc22a3aa6c1e7d825955279de4666
Israel Ortiz <soy@israelortiz.com>
2019-11-02 15:53:23 -0600

    feat: importamos componente nav
    y quitamos fixed top, no se necesita porque en los estilos ya está margin-bottom -56px

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Portada from "./components/Portada.svelte";
   import Contacto from "./components/Contacto.svelte";
+  import Nav from "./components/Nav.svelte";
 </script>
 
+<Nav />
 <Portada />
 <Contacto />
