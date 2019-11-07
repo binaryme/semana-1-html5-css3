0672b1c3c5b9a7b6d33ac7be4a72e494b2844d0e
Israel Ortiz <soy@israelortiz.com>
2019-11-07 10:33:16 -0600

    configuration sapper con bootstrap importado

---
+++ blog/src/components/Navbar.svelte
@@ -0,0 +1,53 @@
+<style>
+  .navbar {
+    margin-bottom: -56px;
+  }
+  .navbar a {
+    color: #ffffff;
+  }
+
+  @media screen and (max-width: 768px) {
+    ul.navbar-nav {
+      text-align: right;
+    }
+  }
+</style>
+
+<nav class="navbar bg-transparent navbar-dark bg-inverse navbar-expand-md">
+  <a class="navbar-brand" href="/">CODEANDLEARN</a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarText"
+    aria-controls="navbarText"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon" />
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="mr-auto" />
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">
+          <i class="fa fa-linkedin" />
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">
+          <i class="fa fa-instagram" />
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">
+          <i class="fa fa-facebook" />
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">
+          <i class="fa fa-youtube" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ blog/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Portada from "../components/Portada.svelte";
+</script>
+
+<svelte:head>
+  <title>Code and learn, blog with style</title>
+</svelte:head>
+
+<Portada />
