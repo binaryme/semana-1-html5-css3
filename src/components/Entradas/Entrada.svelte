3c5e0c902339674133eb7d744fb960fe5717b8e4
Israel Ortiz <soy@israelortiz.com>
2019-11-08 11:03:27 -0600

    allow entering to the blog from index

--- src/components/Entradas/Entrada.svelte
+++ src/components/Entradas/Entrada.svelte
@@ -1,3 +1,9 @@
+<script>
+  export let title;
+  export let html;
+  export let slug;
+</script>
+
 <style>
   h2 {
     font-weight: 600;
@@ -12,14 +18,15 @@
     line-height: 20px;
     font-weight: 400;
     color: rgba(0, 0, 0, 0.54);
-    margin-top: 15px;
+    margin-top: 10px;
   }
-  a {
+  a.author {
     color: #292929;
     font-weight: 100;
     font-size: 14px;
     margin-top: 20px;
     display: inline-block;
+    z-index: 100;
   }
   a:hover {
     text-decoration: none;
@@ -28,18 +35,39 @@
     font-size: 14px;
     color: #757575;
   }
+  img {
+    object-fit: cover;
+    width: 200px;
+    height: 200px;
+  }
+  .go-to-next-page {
+    position: absolute;
+    width: 100%;
+    height: 90%;
+    color: transparent;
+    z-index: 50;
+    top: 0;
+  }
+  .go-to-next-page:hover {
+    cursor: pointer;
+  }
 </style>
 
 <article class="row mt-30">
   <div class="col-lg-8">
-    <h2>Cómo conseguir tu primer trabajo como programador front-end</h2>
+    <h2>{title}</h2>
     <h3>y 3 reglas para obtener un buen sueldo</h3>
-    <a href="https://educatum.co" rel="author">Israel Ortiz Cortés</a>
+    <a href="https://educatum.co" rel="author" class="author" target="_blank">
+      Israel Ortiz Cortés
+    </a>
     <p>
       <time datetime="2008-02-14 20:00">8 noviembre 2019</time>
     </p>
+    <a href={`/blog/${slug}`} alt="go to next page" class="go-to-next-page">
+      go to next page
+    </a>
   </div>
-  <div class="col-lg-4">
+  <div class="col-lg-4 d-flex justify-content-left">
     <img
       src="https://firebasestorage.googleapis.com/v0/b/blog-81f38.appspot.com/o/two-women-looking-at-the-code-at-laptop.jpg?alt=media&token=3e516928-b44e-4ba8-b518-8e081df2bb3b"
       alt="imagen-ilustrativa"
